893144b58c5becc45177f207e78438eefd62cc93
Nari Roh <nari.rohh@gmail.com>
2018-06-11 18:59:44 -0500

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Geckos-0!😎</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
