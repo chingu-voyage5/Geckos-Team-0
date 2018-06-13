11c5b568150ae97b2ee9cb0cca0b38c82c668efa
Nari Roh <nari.rohh@gmail.com>
2018-06-12 22:38:33 -0500

    Add weather modal and styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import '../styles/Weather.css';
+
 import Shower from "react-icons/lib/ti/weather-shower";
+import ReactModal from "react-modal";
 
 class Weather extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false,
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
   render() {
-    return <div id="Weather">
-				<div className="Weather__Row">
+    return (
+      <div id="Weather">
+				<div className="Weather__Row" onClick={this.handleOpenModal}>
 					<Shower />
 					<Degree />
 				</div>
 				<div className="Weather__Row">
 					<Location />
 				</div>
-			</div>;
+        <ReactModal
+          isOpen={this.state.showModal} 
+          onRequestClose={this.handleCloseModal}
+          className="Modal"
+          overlayClassName="Overlay" 
+        >
+          <p>Weather</p>
+        </ReactModal>
+      </div>
+    );
   }
 }
 
