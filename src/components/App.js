2bd547aead06cbc9a36b2898f83f6a44f353870e
Nari Roh <nari.rohh@gmail.com>
2018-06-13 18:38:14 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,21 +11,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-				<div className="App__Top">
-					<Links />
-					<Weather />
-				</div>
-				<div className="App__Center">
-					<span>
-	          <Time />
-					</span>
-					<ToDoInput />
-				</div>
-				<div className="App__Footer">
-					<h3>Setting button Here</h3>
+	<div className="App__Top">
+	  <Links />
+	  <Weather />
+	</div>
+	<div className="App__Center">
+	  <span>
+	    <Time />
+	  </span>
+	  <ToDoInput />
+        </div>
+	<div className="App__Footer">
+	  <h3>Setting button Here</h3>
           <h3>Quotes Here</h3>
           <h3>Todo Here</h3>
-				</div>
+	</div>
       </div>
     );
   }
