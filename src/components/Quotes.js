1d4b0152b77d94469438658281f9b563c1c7f15f
Nari Roh <nari.rohh@gmail.com>
2018-06-14 17:48:32 -0500

    Style Quote and Action components

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,51 +1,57 @@
 import React from "react";
 import "../styles/Quotes.css";
 
-import { FaTwitter, FaHeartO, FaHeart } from "react-icons/lib/fa";
+import { TiHeartOutline, TiHeartFullOutline} from "react-icons/lib/ti";
+import FaTwitter from "react-icons/lib/fa/twitter";
+import GoQuote from "react-icons/lib/go/quote";
 
 class Quotes extends React.Component {
-  render() {
-    return (
-      <div id="Quotes">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	render() {
+		return (
+			<div id="Quotes">
+				<div className="Quote__Container">
+					<span>
+						<GoQuote />
+					</span>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
         <Actions />
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 class Actions extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isFull: false
-    };
+	constructor() {
+		super();
+		this.state = {
+			isFull: false
+		};
 
-    this.toggleHeart = this.toggleHeart.bind(this);
-  }
+		this.toggleHeart = this.toggleHeart.bind(this);
+	}
 
-  toggleHeart() {
-    this.setState({
-      isFull: !this.state.isFull
-    });
-  }
+	toggleHeart() {
+		this.setState({
+			isFull: !this.state.isFull
+		});
+	}
 
-  render() {
-    const { isFull } = this.state;
-  
-    return (
+	render() {
+		const { isFull } = this.state;
+
+		return (
       <div id="Actions">
-        <span id="Name">Author Name</span>
-        <span 
-          className="Heart" 
-          onClick={this.toggleHeart}
-        >
-          { isFull ? <FaHeart /> : <FaHeartO /> }
-        </span>
-        <span id="Twitter"><FaTwitter /></span>
+				<span id="Name">Author Name</span>
+				<span className="Heart" onClick={this.toggleHeart}>
+          {isFull ? <TiHeartFullOutline /> : <TiHeartOutline />}
+				</span>
+				<span id="Twitter">
+					<FaTwitter />
+				</span>
       </div>
     );
-  }
+	}
 }
 
 export default Quotes;
