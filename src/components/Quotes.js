4f3b75de3349a0f4e2c75d9c3f8f1d507b7db70a
Nari Roh <nari.rohh@gmail.com>
2018-06-14 16:51:02 -0500

    Add Action component

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React from "react";
 import "../styles/Quotes.css";
+
+import { FaTwitter, FaHeartO, FaHeart } from "react-icons/lib/fa";
 
 class Quotes extends React.Component {
   render() {
     return (
       <div id="Quotes">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <div className="Quotes__Bottom">
-        </div>
+        <Actions />
+      </div>
+    );
+  }
+}
+
+class Actions extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isFull: false
+    };
+
+    this.toggleHeart = this.toggleHeart.bind(this);
+  }
+
+  toggleHeart() {
+    this.setState({
+      isFull: !this.state.isFull
+    });
+  }
+
+  render() {
+    const { isFull } = this.state;
+  
+    return (
+      <div id="Actions">
+        <span id="Name">Author Name</span>
+        <span 
+          className="Heart" 
+          onClick={this.toggleHeart}
+        >
+          { isFull ? <FaHeart /> : <FaHeartO /> }
+        </span>
+        <span id="Twitter"><FaTwitter /></span>
       </div>
     );
   }
