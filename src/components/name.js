ff1f061ee3b541a758869ff37797d5105ac69651
kiranshuaib <kiran.shuaib@gmail.com>
2018-06-14 13:34:55 -0400

    name.js modified'

--- src/components/name.js
+++ src/components/name.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {Time} from './time';
+import {Time} from './time';
 import '../styles/Name.css';
 
 
@@ -8,20 +8,26 @@ export class Name extends React.Component {
     constructor(props){
         super(props)
             this.state = {
-              html: "___" 
+              html: " " 
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange = evt => {
+        evt.preventDefault();
         this.setState({ html: evt.target.value });
+        
     };
+
+    handleTime(){
+
+    }
 
     render() {
         return <div id="greeting" className="transition">
 				<h2>
-					Good <span className>
-                    morning
+					Good <span> 
+                        morning
                     </span>,
 					<span className = "single-line" contentEditable={true}  onChange={this.onChange}>
 						{this.state.html}
