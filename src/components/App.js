daefeab90a24522a35b7497b3a479a452f8b57c6
kiranshuaib <kiran.shuaib@gmail.com>
2018-06-14 09:50:19 -0400

    app.js and name.js updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,24 @@
-import React, { Component } from "react";
-import "../styles/App.css";
-import "../styles/time.css";
+import React, { Component } from 'react';
+import '../styles/App.css';
+import '../styles/time.css';
 
-import { Time } from "./time.js";
-import ToDoInput from "./ToDoInput.js";
-import Weather from "./Weather";
-import Links from "./Links";
-import Search from "./Search";
+import {Time} from './time.js';
+import {Name} from './name.js';
+import ToDoInput from './ToDoInput.js';
+import Weather from './Weather';
 
 class App extends Component {
+ 
+  
   render() {
+   
     return (
       <div className="App">
-<<<<<<< HEAD
         <Time />
         <Weather />
         <ToDoInput />
-=======
-        <div className="App__Top">
-          <div className="APP__Top__Left">
-            <Links />
-            <Search />
-          </div>
-          <Weather />
-        </div>
-        <div className="App__Center">
-          <span>
-	    <Time />
-          </span>
-          <ToDoInput />
-        </div>
-        <div className="App__Footer">
-          <h3>Setting button Here</h3>
-          <h3>Quotes Here</h3>
-          <h3>Todo Here</h3>
-        </div>
->>>>>>> origin
       </div>
-    );
+    )
   }
 }
 
--- src/components/name.js
+++ src/components/name.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
 
-export class Time extends Component {
+export class Name extends Component {
+
 }
