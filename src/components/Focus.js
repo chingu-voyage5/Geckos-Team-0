26f28985d433be354ff65ad0c951c70d65d9403d
kiranshuaib <kiran.shuaib@gmail.com>
2018-06-15 16:36:36 -0400

    fixed

--- src/components/Focus.js
+++ src/components/Focus.js
@@ -6,11 +6,8 @@ class Focus extends React.Component {
       return (
         <div className="ToDoInput">
             <form>
-<<<<<<< HEAD:src/components/ToDoInput.js
                   <p>what is you main focus today</p>
-=======
                 <h1>What is your main focus for today?</h1>
->>>>>>> origin:src/components/Focus.js
                 <input type="text" name="todo"/>
             </form>
         </div>
