2ce80cee32178614af98fdc38e986fbe8171e9d0
kiranshuaib <kiran.shuaib@gmail.com>
2018-06-16 11:55:38 -0400

    name changed time.js to Time.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,17 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import '../styles/App.css';
 import '../styles/Time.css';
-=======
-import React, { Component } from "react";
-import "../styles/App.css";
->>>>>>> 0e21c2263223d94b484eb7bbbb76aea52c41cd98
 
 
 import {Time} from './Time';
 import {Name} from './Name';
 import Weather from './Weather';
-<<<<<<< Updated upstream
-
-import "../styles/time.css";
-
-
 import Focus from "./Focus";
-import ToDo from "./ToDo.js";
-
-=======
-import Focus from "./Focus";
-import ToDo from "./ToDo.js";
->>>>>>> Stashed changes
+import ToDo from "./ToDo.js";P
 import Links from "./Links";
 import Search from "./Search";
-import Quotes from "./Quotes";
 
 class App extends Component {
-<<<<<<< HEAD
  
   
   render() {
@@ -57,32 +40,11 @@ class App extends Component {
         <div className="App__Footer">
           <h3>Setting button Here</h3>
           <h3>Quotes Here</h3>
-=======
-	render() {
-		return (
-			<div className="App">
-				<div className="App__Top">
-					<div className="APP__Top__Left">
-						<Links />
-						<Search />
-					</div>
-					<Weather />
-				</div>
-				<div className="App__Center">
-					<span>
-						<Time />
-					</span>
-					<Focus />
-				</div>
-				<div className="App__Footer">
-					<h3>Setting button Here</h3>
-					<Quotes />
->>>>>>> 0e21c2263223d94b484eb7bbbb76aea52c41cd98
 					<ToDo />
-				</div>
-			</div>
-		);
-	}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
