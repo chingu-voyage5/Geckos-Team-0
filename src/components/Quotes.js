41ca361910bdc79f388e17cfb282a4b1ad069c02
Nari Roh <nari.rohh@gmail.com>
2018-06-15 20:54:17 -0500

    Add hover effect

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,21 +1,41 @@
 import React from "react";
 import "../styles/Quotes.css";
 
-import { TiHeartOutline, TiHeartFullOutline} from "react-icons/lib/ti";
+import { TiHeartOutline, TiHeartFullOutline } from "react-icons/lib/ti";
 import FaTwitter from "react-icons/lib/fa/twitter";
 import GoQuote from "react-icons/lib/go/quote";
 
 class Quotes extends React.Component {
+	constructor() {
+		super();
+		this.state = { isHovered: false };
+
+		this.toggleHover = this.toggleHover.bind(this);
+	}
+
+	toggleHover() {
+		this.setState({ isHovered: !this.state.isHovered });
+	}
+
 	render() {
+		const actionClass = this.state.isHovered ? "slide-down" : "";
+		const quoteClass = this.state.isHovered ? "slide-up" : "";
 		return (
-			<div id="Quotes">
-				<div className="Quote__Container">
+			<div
+				id="Quotes"
+				onMouseEnter={this.toggleHover}
+				onMouseLeave={this.toggleHover}
+			>
+				{/* 👇 When mouse enters #Quotes, add "slide-down" and "slide-up" classes */}
+				<div className={`Quote__Container ${quoteClass}`}>
 					<span>
 						<GoQuote />
 					</span>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 				</div>
-        <Actions />
+				<div className={`Action__Container ${actionClass}`}>
+					<Actions />
+				</div>
 			</div>
 		);
 	}
@@ -24,33 +44,29 @@ class Quotes extends React.Component {
 class Actions extends React.Component {
 	constructor() {
 		super();
-		this.state = {
-			isFull: false
-		};
+		this.state = { isFull: false };
 
 		this.toggleHeart = this.toggleHeart.bind(this);
 	}
 
 	toggleHeart() {
-		this.setState({
-			isFull: !this.state.isFull
-		});
+		this.setState({ isFull: !this.state.isFull });
 	}
 
 	render() {
 		const { isFull } = this.state;
 
 		return (
-      <div id="Actions">
+			<div id="Actions">
 				<span id="Name">Author Name</span>
 				<span className="Heart" onClick={this.toggleHeart}>
-          {isFull ? <TiHeartFullOutline /> : <TiHeartOutline />}
+					{isFull ? <TiHeartFullOutline /> : <TiHeartOutline />}
 				</span>
 				<span id="Twitter">
 					<FaTwitter />
 				</span>
-      </div>
-    );
+			</div>
+		);
 	}
 }
 
