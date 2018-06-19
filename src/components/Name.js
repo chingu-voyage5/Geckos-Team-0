c1c3b36e273535c930e665e0956cf117b113e2b1
kiranshuaib <kiran.shuaib@gmail.com>
2018-06-19 17:26:07 -0400

    add local storage so that name remains when page refreshes

--- src/components/Name.js
+++ src/components/Name.js
@@ -8,16 +8,21 @@ export class Name extends React.Component {
     constructor(props){
         super(props)
             this.state = {
-              html: " " 
+              html: "  " ,
+            
         }
-        this.handleChange = this.handleChange.bind(this);
+      
     }
 
 
     handleChange = evt => {
         evt.preventDefault();
-        this.setState({ html: evt.target.value });
-        console.log(Time.getTimeString());
+        this.setState(
+            { html: evt.target.value  }
+    );
+        
+        // add local storage so that name remains when page refresh.
+        localStorage.getItem('name', this.html);
     };
 
   
@@ -26,13 +31,12 @@ export class Name extends React.Component {
         return <div id="greeting" className="transition">
 				<h2>
 					Good <span> 
-                        
-                        morning
+                    morning
                     </span>,
 	
-                    
-                <span className="single-line" ref={function (e) { if (e != null) e.contentEditable = true; }} onChange={this.onChange}>
-                    {this.state.html}
+
+                <span className="single-line" ref={function (e) { if (e != null) e.contentEditable = true; }} onChange={this.onChange} spellCheck= "false">
+                    {   this.state.html}
                         .
 					</span>
 				</h2>
