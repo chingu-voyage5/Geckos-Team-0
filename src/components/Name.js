7bd5b4b4914fb880c966194ef531498d6a727149
kiranshuaib <kiran.shuaib@gmail.com>
2018-06-22 13:17:59 -0400

    added name to local storage using Json stringify and Json Parse methods

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/Name.css';
+import ReactDOM from 'react-dom';
+
 
 
 
@@ -12,44 +14,91 @@ export class Name extends React.Component {
       this.handleChange = this.handleChange.bind(this);
     }
 
-
-    // handleChange = evt => {
-    //     evt.preventDefault();
-    //     console.log(evt.target.value);
-    //     this.setState({ html: evt.target.value });
-    //     console.log(evt);
-    //     // console.log(this.state.html)
-      
-    // };
+    handleChange(event) {
+        
+        this.setState({ html: event.target.value });
+        console.log(this.state.html);
+    }
 
 
-    handleChange = (evt) =>{
-      
-        console.log(evt.target.value);
-       
-
-    };
+    
  
+    componentDidMount() {
+        const json = localStorage.getItem('name');
+        console.log(json);
+        const html = JSON.parse(json);
 
+      
 
-    render() {
-        return <div id="greeting" className="transition">
-            <h2>
-                Good <span>
-                    morning
-                    </span>,
-					{/* <span className="single-line" ref={function (e) { if (e != null) e.contentEditable = true; }} onBlur={this.handleChange}> */}
-					<span className="single-line"  contentEditable = "true";  onBlur={this.handleChange}>
-                  {this.state.html}
-					</span>
-                    .
-					</span>
-            </h2>
-        </div>;
-
-
-
+       if(html){
+        this.setState(() => ({ html: html }));
+       }
+        console.log(html);
 
     }
 
+
+
+   componentDidUpdate(prevState){
+       if(prevState.html !== this.state.html){
+       const json = JSON.stringify(this.state.html);
+       console.log(json)
+       localStorage.setItem('name', json);
+       console.log('Saving Data');
+       }
+   }
+
+
+
+    render = () => {
+        return (
+        <div id="greeting" className="transition">
+				<h2>
+					Good <span>morning</span>,
+                
+                   <ContentEditable html={this.state.html} // innerHTML of the editable div
+                        disabled={false}       // use true to disable edition
+                        onChange={this.handleChange} 
+                         />
+					.
+				</h2>
+            </div>
+        )
+
+    }
 }
+
+    class ContentEditable extends React.Component{
+        constructor(){
+            super();
+            this.emitChange = this.emitChange.bind(this);
+
+        }
+             render(){
+            return <span
+             className="single-line"
+             onInput={this.emitChange} 
+             onBlur={this.emitChange}
+             ref={function (e) { if (e != null) e.contentEditable = true; }}
+             dangerouslySetInnerHTML={{ __html: this.props.html }}> 
+             </span>
+             }
+    
+    
+             shouldComponentUpdate(nextProps){
+                 return nextProps.html !== ReactDOM.findDOMNode(this).innerHTML;
+            }
+      
+             emitChange(){
+                 var html = ReactDOM.findDOMNode(this).innerHTML;
+                 if (this.props.onChange && html !== this.lastHtml) {
+
+                     this.props.onChange({
+                         target: {
+                             value: html
+                         }
+                     });
+                 }
+                 this.lastHtml = html;
+             }
+    }
