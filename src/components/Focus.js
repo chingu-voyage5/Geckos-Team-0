fdd3bb406ef3d4c17b9616205d0e903f99124074
Jon Maldia <jon@maldia.net>
2018-06-28 18:27:58 -0400

    Adds Lifecycle methods to Focus.js

--- src/components/Focus.js
+++ src/components/Focus.js
@@ -2,12 +2,39 @@ import React from 'react';
 import "../styles/Weather.css";
 
 class Focus extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            focus: '' 
+        };
+    }
+
+    componentDidMount() {
+        try {
+          const json = localStorage.getItem('focus');
+          const options = JSON.parse(json);
+      
+          if (options) {
+            // this.setState(() => ({ focus }));
+          }
+        } catch (e) {
+          // Do nothing
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.focus !== this.state.focus.length) {
+            localStorage.setItem('focus', this.state.focus);
+        }
+        console.log('It Updated!');
+    }
+
     render() {
       return (
-        <div className="ToDoInput">
+        <div className="FocusInput">
             <form>
                 <h1>What is your main focus for today?</h1>
-                <input type="text" name="todo"/>
+                <input id="FocusValue" type="text" name="todo" value={'Value'}/>
             </form>
         </div>
       );
