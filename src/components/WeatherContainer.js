7792e43ef5f6e1dfd4800bd4e12ee237b98ec733
Nari Roh <nari.rohh@gmail.com>
2018-07-06 13:33:27 -0500

    Get 5days weather forcast

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,19 +1,15 @@
 import React, { Fragment } from "react";
 import Weather from "./Weather";
+import Store from "../store.js";
 import "../styles/Weather.css";
 
 class WeatherContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			// currentWeather: {
-				id: "0",
-				city: null,
-				region: null,
-				countryCode: null,
-				temperature: null,
-				weather: null
-			// }
+      location: {},
+      currentWeather: {},
+      forecastWeather: {}
 		};
 	}
 
@@ -39,39 +35,87 @@ class WeatherContainer extends React.Component {
 			.then(response => response.json())
 			.then(json => {
 				console.log(json);
-				let data = json.query.results.channel;
-				this.setState({
-					id: data.item.condition.code,
-					city: data.location.city,
-					countryCode: data.title.split(", ").pop(),
-					temperature: data.item.condition.temp,
-					weather: data.item.condition.text
-				});
-				console.log(
-					this.state.temperature,
-					this.state.weather,
-					this.state.id,
-					this.state.city,
-					this.state.countryCode,
-					json
-				);
+        let data = json.query.results.channel;
+        let { forecast } = data.item;
+        // location 
+        // countryCode: data.title.split(", ").pop(),
+				// city: data.location.city,
+        // 
+        // currentWeather 
+        // weatherCode: data.item.condition.code,
+        // temperature: data.item.condition.temp,
+        // weather: data.item.condition.text
+        this.setState({
+          location: {
+            countryCode: data.title.split(", ").pop(),
+            city: data.location.city
+          },
+          currentWeather: {
+            weatherCode: data.item.condition.code,
+            temperature: data.item.condition.temp,
+            weather: data.item.condition.text
+          },
+          forecastWeather: {
+            day1: forecast[0],
+            day2: forecast[1],
+            day3: forecast[2],
+            day4: forecast[3],
+            day5: forecast[4]
+          },
+        });
+        console.log(this.state)
+
+				// this.setState({
+        //   weatherCode: data.item.condition.code,
+				// 	city: data.location.city,
+				// 	countryCode: data.title.split(", ").pop(),
+				// 	temperature: data.item.condition.temp,
+				// 	weather: data.item.condition.text
+				// });
+				// console.log(
+				// 	this.state.temperature,
+				// 	this.state.weather,
+				// 	this.state.id,
+				// 	this.state.city,
+				// 	this.state.countryCode,
+				// 	json
+				// );
 			});
 	};
 
 	render() {
-		const { id, city, countryCode, weather, temperature } = this.state;
 		// console.log(city, country, weather, temperature);
 		return ( 
       <Fragment>
-        <Weather 
+        <p>hi</p>
+        {/* <Weather 
           city={city}
           country={countryCode}
           weather={weather}
           temp={temperature}
-        />
+        /> */}
       </Fragment>
     );
 	}
 }
 
 export default WeatherContainer;
+
+/*
+		this.state = {
+			currentWeather: {
+				weatherCode: "0",
+				city: null,
+				region: null,
+				countryCode: null,
+				temperature: null,
+				weather: null
+      },
+      forecastWeather: {
+        day1: currentWeather,
+        day2: {},
+        day3: {},
+        day4: {},
+        day5: {}
+      }
+*/
