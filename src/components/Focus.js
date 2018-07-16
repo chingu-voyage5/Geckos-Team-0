a13d23f3fdaf4b8a77d08de944e19ab7ae325a76
Jon Maldia <jon@maldia.net>
2018-07-15 22:37:56 -0400

    Adds focus functions - add, delete. Styles

--- src/components/Focus.js
+++ src/components/Focus.js
@@ -8,18 +8,20 @@ class Focus extends React.Component {
             focus: '' 
         };
 
-        this.handleChange = this.handleChange.bind(this);
         this.addFocus = this.addFocus.bind(this);
         this.deleteFocus = this.deleteFocus.bind(this);
+        this.strikeText = this.strikeText.bind(this);
     }
 
     componentDidMount() {
         try {
-          const json = localStorage.getItem('focus');
-          const options = JSON.parse(json);
+          const value = localStorage.getItem('focus');
+          
       
-          if (options) {
-            // this.setState(() => ({ focus }));
+          if (value) {
+            this.setState({
+                focus: value
+            });
           }
         } catch (e) {
           // Do nothing
@@ -28,56 +30,69 @@ class Focus extends React.Component {
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.focus !== this.state.focus.length) {
-            const html = this.state.focus;
-
-            localStorage.setItem('focus', html);
-            console.log('I\'m here', html);
+            localStorage.setItem('focus', this.state.focus);
         }
-        console.log('It Updated!');
+        console.log(`Focus is ${this.state.focus}`);
     }
 
     // Grabs the input
-    handleChange(e) {
-        this.setState({
-            focus: e.target.value
-        });
-        console.log(`change handled`);
-    }
+    addFocus(e) {
+        e.preventDefault();
 
-    // Changes the focus item 
-    addFocus(focus) {
-        if (focus.length > 0) {
-            this.props.addFocus(focus);
-            this.setState({focus: ''});
-        }
+        const { value } = this.input;
+
+        if (value === '') { return }
+
+        this.setState({
+            focus: value
+        });
+
         console.log(`focus added`);
     }
 
     // Handles removal of Focus field
     deleteFocus(e) {
+        this.setState({
+            focus: ''
+        });
         console.log(`focus deleted`);
+    }
+
+    // Strikethrough text
+    strikeText() {
+        console.log(`Checkbox clicked`);
     }
 
     render() {
       return (
         <div>   
-            <div className="FocusInput">
-                <form onSubmit={() => this.addFocus(this.state.value)}>
-                    <p id="FocusHeader">What is your main focus for today?</p>
-                    <input 
-                        id="  " 
-                        type="text" 
-                        name="focus" 
-                        onChange={this.handleChange}
-                        value={this.state.focus}
-                    />
-                </form>
-            </div>
-            <div className="FocusInput ">
-                <p id="FocusItemHeader">TODAY</p>
-                <input type="checkbox" id="FocusCheck" name="focus-check" />
-                <p id="FocusItem"  >{this.state.focus}</p>
-            </div>
+            {this.state.focus === '' && (
+                <div className="FocusInput">
+                    <form onSubmit={this.addFocus}>
+                        <p id="FocusHeader">What is your main focus for today?</p>
+                        <input 
+                            id="FocusValue" 
+                            type="text" 
+                            name="focus" 
+                            ref={node => this.input = node}
+                        />
+                    </form>
+                </div>
+            )}
+            {this.state.focus !== '' && (
+                <div className="FocusInput">
+                    <p id="FocusItemHeader">TODAY</p>
+                    <div className="FocusParent">
+                        <div id="CheckboxItem" onClick={this.strikeText}>
+                            <input type="checkbox" id="FocusCheck" name="focus-check" />
+                            <label htmlFor="FocusCheck"></label>
+                        </div>
+                        <p id="FocusItem"  >{this.state.focus}</p>
+                        <p id="DeleteItem" onClick={this.deleteFocus}>x</p>
+                    </div>
+                </div>
+            )}
+            
         </div>
       );
     }
