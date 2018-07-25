ada44984b17be913c0be2d6190c7a2084e4e20ea
Jon Maldia <jon@maldia.net>
2018-07-25 12:22:07 -0400

    Add variables for the background function that randomizes the images per day

---
+++ src/components/Background.js
@@ -0,0 +1,9 @@
+function changeBackground() {
+    let date = date.now();
+    let backgrounds = [
+        { name: 'White Sand Beach', author: 'Monica Silvestre', location: 'src/img/white_beach_monica_silvestre.jpeg' },
+        { name: 'Toronto', author: 'Next Voyage', location: 'src/img/toronto_next_voyage.jpeg' },
+        { name: 'Snow Covered Mountains by Lake', author: 'Ricardo Bresciani', location: 'src/img/snow_mountains_by_ricardo_bresciani.jpeg' },
+        { name: 'Boats', author: 'Callebe Miranda', location: 'src/img/boats_by_callebe_miranda.jpeg' }
+    ];
+}
