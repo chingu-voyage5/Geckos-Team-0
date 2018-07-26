6ade9b1afbe66fe7c0da4458da221194d0ce2643
Nari Roh <nari.rohh@gmail.com>
2018-07-26 18:58:08 -0500

    Display background name & author

--- src/components/WallpaperInfo.js
+++ src/components/WallpaperInfo.js
@@ -6,29 +6,57 @@ import { TiHeartOutline, TiHeartFullOutline } from "react-icons/lib/ti";
 class WallpaperInfo extends React.Component {
 	constructor() {
 		super();
-		this.state = { isFull: false };
+		this.state = { 
+      isClicked: false,
+      wallpaperName: "",
+      wallpaperAuthor: ""
+    };
 
 		this.toggleHeart = this.toggleHeart.bind(this);
-	}
+  }
+
+  componentDidMount() {
+    this.getWallpaperInfo();
+  }
+  
+  getWallpaperInfo = async () => {
+    try {
+      const wallpaperName = await localStorage.getItem("backgroundName");
+      const wallpaperAuthor = await localStorage.getItem("backgroundAuthor");
+      
+      if (wallpaperName && wallpaperAuthor) {
+        this.setState({
+          wallpaperName,
+          wallpaperAuthor
+        });
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
 	toggleHeart() {
-    this.setState({ isFull: !this.state.isFull });
+    this.setState({ isClicked: !this.state.isClicked });
   }
   
 	render() {
-    const { isFull } = this.state;
+    const { isClicked, wallpaperName, wallpaperAuthor } = this.state;
 
 		return (
       <div id="WallpaperInfo">
         <div className="place">
-          <span>Mexico city, Mexico</span>
+          <span>
+            {wallpaperName}
+          </span>
         </div>
 				<div className="source">
           <a className="wiki-link" href="#"> 
-            <span className="name">Photo by, Full Name</span>
+            <span className="name">
+              Photo by, {wallpaperAuthor}
+            </span>
           </a>
 					<span className="icon" onClick={this.toggleHeart}>
-						{isFull ? <TiHeartFullOutline /> : <TiHeartOutline />}
+						{isClicked ? <TiHeartFullOutline /> : <TiHeartOutline />}
 					</span>
 				</div>
       </div>
