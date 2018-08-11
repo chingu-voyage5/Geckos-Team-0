e7207448af076cb8a52d967155f95756f87172ae
Nari Roh <nari.rohh@gmail.com>
2018-08-10 23:38:16 -0500

    Fix indentation

--- src/components/MainWeather.js
+++ src/components/MainWeather.js
@@ -8,38 +8,34 @@ import WeatherIcon from "react-icons-weather";
 function MainWeather({ handleOpenModal }) {
 
     return (
-    	<div
-      		className="Weather--Main"
-      		onClick={() => handleOpenModal()}
-    	>
-        <Store.Consumer>
-        	{store => {
-            	const { weatherCode, temperature } = store.currentWeather;
-           	 	const { city, countryCode } = store.location;
-            	const { unit } = store;
+    	<div className="Weather--Main" onClick={() => handleOpenModal()}>
+            <Store.Consumer>
+                {store => {
+                    const { weatherCode, temperature } = store.currentWeather;
+                    const { city, countryCode } = store.location;
+                    const { unit } = store;
 
-           		return (
-            		<Fragment>
-						<div className="Weather--Main__Row">
-							<WeatherIcon name="yahoo" iconId={weatherCode || "32"} />
-							<div className="temp">
-								<span>
-									{unit ? store.convertToC(temperature) : temperature}°
-								</span>
-							</div>
-						</div>
-						<div className="Weather--Main__Row">
-							<div className="location">
-								<span>
-									{city}{countryCode}
-								</span>
-							</div>
-						</div>
-              		</Fragment>
-           		);
-            }      
-        }
-        </Store.Consumer>
+                    return (
+                        <Fragment>
+                            <div className="Weather--Main__Row">
+                                <WeatherIcon name="yahoo" iconId={weatherCode || "32"} />
+                                <div className="temp">
+                                    <span>
+                                    {unit ? store.convertToC(temperature) : temperature}°
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="Weather--Main__Row">
+                                <div className="location">
+                                    <span>
+                                        {city}{countryCode}
+                                    </span>
+                                </div>
+                            </div>
+                        </Fragment>
+                    );
+                }}
+            </Store.Consumer>
         </div>
     );
 }
