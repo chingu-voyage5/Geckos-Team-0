605ade251cf04e9da6635e57c7e8f151b735900d
Nari Roh <nari.rohh@gmail.com>
2018-08-10 23:30:13 -0500

    Fix indentaion

--- src/components/App.js
+++ src/components/App.js
@@ -13,32 +13,32 @@ import WallpaperInfo from "./WallpaperInfo";
 import WeatherContainer from "./WeatherContainer";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<div className="App__Top">
-					<div className="APP__Top__Left">
-						<Links />
-						<Search />
-					</div>
-         			<WeatherContainer />
-				</div>
-				<div className="App__Center">
-					<Time />
-					<Name />
-					<Focus />
-				</div>
-				<div className="App__Footer">
-					<div className="App__Footer__Left">
-						<Settings />
-						<WallpaperInfo />
-					</div>
-        			<QuoteContainer />
-					<ToDo />
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="App">
+                <div className="App__Top">
+                    <div className="APP__Top__Left">
+                        <Links />
+                        <Search />
+                    </div>
+                    <WeatherContainer />
+                </div>
+                <div className="App__Center">
+                    <Time />
+                    <Name />
+                    <Focus />
+                </div>
+                <div className="App__Footer">
+                    <div className="App__Footer__Left">
+                        <Settings />
+                        <WallpaperInfo />
+                    </div>
+                    <QuoteContainer />
+                    <ToDo />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
